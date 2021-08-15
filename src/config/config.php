31ff6487f0c640276d40799b457490fedf0471a2
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-08-15 19:20:10 -0300

    login validation view

--- src/config/config.php
+++ src/config/config.php
@@ -6,9 +6,12 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 // Pastas 
 define('MODELS_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEWS_PATH', realpath(dirname(__FILE__) . '/../views'));
+define('TEMPLATES_PATH', realpath(dirname(__FILE__) . '/../views/template'));
 define('CONTROLLERS_PATH', realpath(dirname(__FILE__) . '/../controllers'));
+define('EXCEPTIONS_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 
 // Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
 require_once(realpath(dirname(__FILE__) . '/loader.php'));
 require_once(realpath(MODELS_PATH . '/Model.php'));
+require_once(realpath(EXCEPTIONS_PATH . '/AppException.php'));

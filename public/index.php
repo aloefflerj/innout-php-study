ca80d439077e050145a3c0cb63dabb1087aa9d2e
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-08-15 17:14:57 -0300

    select command for model class

--- public/index.php
+++ public/index.php
@@ -4,7 +4,4 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(["name" => "Anderson", "email" => "aloeffler@gmail.com"]);
-print_r($user);
-echo '<br>';
-$user->email = 'matcha@email.com';
-print_r($user->email);
+echo $user->getSelect(["name" => "Chaves", "email" => "chaves@cod3r.com.br"]);

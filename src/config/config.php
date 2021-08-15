f23e680ac699e954a6b0125662197f4258b16248
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-08-15 18:36:18 -0300

    Login model and login validation

--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,9 @@ date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 // Pastas 
-define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('MODELS_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('VIEWS_PATH', realpath(dirname(__FILE__) . '/../views'));
 
+// Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
+require_once(realpath(MODELS_PATH . '/Model.php'));

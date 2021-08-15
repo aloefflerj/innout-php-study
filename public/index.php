bec82c7826beb3665a9bad18abe06cc615e46a7d
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-08-15 17:30:02 -0300

    get users from db

--- public/index.php
+++ public/index.php
@@ -3,5 +3,8 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(["name" => "Anderson", "email" => "aloeffler@gmail.com"]);
-echo $user->getSelect(["name" => "Chaves", "email" => "chaves@cod3r.com.br"]);
+print_r(User::get(['name' => 'Chaves'], 'name, email'));
+
+echo '<br><br>';
+
+echo '<pre>' , print_r(User::get()), '</pre>';

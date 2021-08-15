f709779f229c6724bb678303edd4ba3d2a0a229c
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-08-15 17:59:38 -0300

    authentication page

--- public/index.php
+++ public/index.php
@@ -1,10 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-print_r(User::get(['name' => 'Chaves'], 'name, email'));
-
-echo '<br><br>';
-
-echo '<pre>' , print_r(User::get()), '</pre>';
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');

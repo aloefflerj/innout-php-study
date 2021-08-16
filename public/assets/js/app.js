493947473299c854db782b0517183cb944e9d3ec
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-08-16 15:52:57 -0300

    hiding sidebar on click

---
+++ public/assets/js/app.js
@@ -0,0 +1,7 @@
+(() => {
+    const menuToggle = document.querySelector(".menu-toggle")
+    menuToggle.onclick = e => {
+        const body = document.querySelector("body")
+        body.classList.toggle("hide-sidebar")
+    }
+})()
